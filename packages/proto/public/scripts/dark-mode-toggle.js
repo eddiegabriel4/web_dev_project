ce3b7537e718e375e16162ce797b6f2682a93fa2
eddiegabriel4 <81613773+eddiegabriel4@users.noreply.github.com>
2024-11-09 00:08:41 -0800

    bug fix

--- packages/proto/public/scripts/dark-mode-toggle.js
+++ packages/proto/public/scripts/dark-mode-toggle.js
@@ -1,23 +1,25 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
+document.addEventListener("DOMContentLoaded", () => {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
 
-// applies dark mode based on stored value
-function applyDarkMode(isDarkMode) {
-    if (isDarkMode) {
-        document.body.classList.add('dark-mode');
-        darkModeToggle.checked = true;
-    } else {
-        document.body.classList.remove('dark-mode');
-        darkModeToggle.checked = false;
+    // Applies dark mode based on stored value
+    function applyDarkMode(isDarkMode) {
+        if (isDarkMode) {
+            document.body.classList.add('dark-mode');
+            darkModeToggle.checked = true;
+        } else {
+            document.body.classList.remove('dark-mode');
+            darkModeToggle.checked = false;
+        }
     }
-}
 
-// checking localStorage for dark mode status on page load
-const isDarkMode = localStorage.getItem('darkMode') === 'true';
-applyDarkMode(isDarkMode);
+    // Checking localStorage for dark mode status on page load
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    applyDarkMode(isDarkMode);
 
-// listening for change, then saving and applying change
-darkModeToggle.addEventListener('change', (event) => {
-    const isChecked = event.target.checked;
-    localStorage.setItem('darkMode', isChecked); 
-    applyDarkMode(isChecked);
+    // Listening for change, then saving and applying change
+    darkModeToggle.addEventListener('change', (event) => {
+        const isChecked = event.target.checked;
+        localStorage.setItem('darkMode', isChecked); 
+        applyDarkMode(isChecked);
+    });
 });
